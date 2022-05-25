23617570531b68476a0d949e5e5114d9104b1b62
TAKIEDONS <takalani@qrspace.io>
2022-05-25 16:52:30 +0200

    2nd commit

--- src/config.py
+++ src/config.py
@@ -1,16 +1,23 @@
-# place where Flask itself puts certain configuration values and also where extensions can put their configuration values. But this is also where you can have your own configuration.
+# src/config.py
+
+
+import os  # new
+
 
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'  # new
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
 
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
